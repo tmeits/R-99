8be28bdcc1c0e8850e5de6408d39b5619803e4a6
tmeits <tmeits@gmail.com>
2013-06-07 14:48:48 +0800

    1.06 (*) Find out whether a list is a palindrome

--- lists.r
+++ lists.r
@@ -38,6 +38,45 @@ r99.1.03.r <- function(lst, k){
 }
 r99.1.03.r(test, 3)
 
+# 1.04 (*) Find the number of elements of a list.
+r99.1.04 <- function(lst){
+  length(lst)
+}
+r99.1.04(test)
+
+# 1.05 (*) Reverse a list.
+r99.1.05 <- function(lst){
+  rev(lst)
+}
+r99.1.05(test)
+
+# 1.06 (*) Find out whether a list is a palindrome. 
+# A palindrome can be read forward or backward; e.g. [x,a,m,a,x].
+
+test<-list('x', 'a', 'm', 'a', 'x')
+test.bad<-list('x', 'a', 'm', 'a', 'z')
+r99.1.06 <- function(lst){
+  #r<-TRUE
+  rlst<-rev(lst)
+  for(i in 1:length(lst)) {
+    ch1<-as.character(lst[i])
+    ch2<-as.character(rlst[i])
+    if(ch1!=ch2) {
+      r<-FALSE
+      #print('Bad')
+      #break
+      return(FALSE)
+    }
+  }
+  #return(r)
+  TRUE
+}
+
+r99.1.06(test)
+r99.1.06(test.bad)
+
+
+
 
 
 
